1a714f191833e67b7da06fca38a53e73095e7415
Maximiliano Luna <maxiespn21@gmail.com>
2023-11-06 15:19:58 -0300

    Create GameTest

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,26 @@
+import org.example.Game;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Scanner;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class GameTest {
+
+    @InjectMocks
+    private Game game;
+
+    @Mock
+    Scanner scanner;
+
+    @Test
+    public void gameTest() {
+        when(scanner.nextLine()).thenReturn("Quit");
+        game.play();
+    }
 }
