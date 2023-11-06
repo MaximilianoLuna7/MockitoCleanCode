9f30bda75b90d1f6ffda8f364ba4f30e0219b5ec
Maximiliano Luna <maxiespn21@gmail.com>
2023-11-06 18:11:31 -0300

    Implement tests for rock, paper, scissors, draw and computer wins

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,10 +1,15 @@
 import org.example.Game;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Random;
 import java.util.Scanner;
 
 import static org.mockito.Mockito.when;
@@ -12,15 +17,97 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class GameTest {
 
+    public static final int OPTION_SCISSORS = 2;
+    public static final int OPTION_PAPER = 1;
+    public static final int OPTION_ROCK = 0;
     @InjectMocks
     private Game game;
 
     @Mock
     Scanner scanner;
 
+    @Mock
+    Random random;
+
     @Test
-    public void gameTest() {
+    public void testCloseGameOnQuit() {
         when(scanner.nextLine()).thenReturn("Quit");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
         game.play();
+
+        Assert.assertTrue(out.toString().contains("Let's play Rock"));
+    }
+
+    @Test
+    public void testRockWinsAgainstScissors() {
+        when(scanner.nextLine()).thenReturn("Rock").thenReturn("Quit");
+        when(random.nextInt(3)).thenReturn(OPTION_SCISSORS);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        game.play();
+
+        Assert.assertTrue(out.toString().contains("Computer chose scissors"));
+        Assert.assertTrue(out.toString().contains("wins:1"));
+    }
+
+    @Test
+    public void testScissorsWinsAgainstPaper() {
+        when(scanner.nextLine()).thenReturn("Scissors").thenReturn("Quit");
+        when(random.nextInt(3)).thenReturn(OPTION_PAPER);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        game.play();
+
+        Assert.assertTrue(out.toString().contains("Computer chose paper"));
+        Assert.assertTrue(out.toString().contains("wins:1"));
+    }
+
+    @Test
+    public void testPaperWinsAgainstRock() {
+        when(scanner.nextLine()).thenReturn("Paper").thenReturn("Quit");
+        when(random.nextInt(3)).thenReturn(OPTION_ROCK);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        game.play();
+
+        Assert.assertTrue(out.toString().contains("Computer chose rock"));
+        Assert.assertTrue(out.toString().contains("wins:1"));
+    }
+
+    @Test
+    public void testDrawWhenBothChooseRock() {
+        when(scanner.nextLine()).thenReturn("Rock").thenReturn("Quit");
+        when(random.nextInt(3)).thenReturn(OPTION_ROCK);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        game.play();
+
+        Assert.assertTrue(out.toString().contains("Computer chose rock"));
+        Assert.assertTrue(out.toString().contains("ties:1"));
+    }
+
+    @Test
+    public void testComputerWins() {
+        when(scanner.nextLine()).thenReturn("Rock").thenReturn("Quit");
+        when(random.nextInt(3)).thenReturn(OPTION_PAPER);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        game.play();
+
+        Assert.assertTrue(out.toString().contains("Computer chose paper"));
+        Assert.assertTrue(out.toString().contains("loses:1"));
     }
 }
